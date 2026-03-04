73d0e8920e4aa003469807643946414bc3e89f66
ekta-2401 <mittitcs@gmail.com>
2026-03-05 00:16:40 +0530

    episode3 - laying the foundation

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {}, "React 🚀"), React.createElement("h2", {}, "I am h2 tag")));
+
+
+
+//React element
+const headingElement = React.createElement("h1", {id: "heading"}, "Heading from React Element");
+const jsxHeading = <h1 id="heading">Heading from JSX</h1>;
+//React functional component
+const HeadingComponent = () => {
+    return <h1 id="heading">Heading from Functional Component</h1>
+}
+
+const Title = () => (
+  <h1 id="title">Heading from Title function</h1>
+);
+
+//shorthand syntax for React functional component
+const HeadingComponent2 = () => <h1 id="heading">Heading from Functional Component 2</h1>
+
+//If we want to write in multiple  line then we have to wrap it in parentheses
+const HeadingComponent3 = () => {
+    return (
+        <div>
+            {Title()}
+            <Title/>
+            {headingElement}   // we can also use React element in functional component
+            {jsxHeading}
+            <HeadingComponent/>
+            <HeadingComponent2/>
+            <p>This is a paragraph from Functional Component 3</p>
+            <h1 id="heading">Heading from Functional Component 3</h1>
+        </div>
+        
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+//root.render(jsxHeading);
+// component rendering
+root.render(<HeadingComponent3 />); 
